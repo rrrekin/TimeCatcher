63c61678f04be8940526c85a216e5fef56804275
Michał Rudewicz <michal.rudewicz@gmail.com>
2025-09-14 11:21:24 +0200

    Build cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,21 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 export default defineConfig({
-    plugins: [vue()],
-    base: './',
-    root: 'src/renderer',
-    build: {
-        outDir: '../../dist/renderer'
-    },
-    server: {
-        port: 5173,
-        strictPort: true
-    },
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, 'src')
-        }
+  plugins: [vue()],
+  base: './',
+  root: 'src/renderer',
+  build: {
+    // Emit to project-level dist/renderer and allow Vite to clean it
+    outDir: resolve(__dirname, 'dist/renderer'),
+    emptyOutDir: true
+  },
+  server: {
+    port: 5173,
+    strictPort: true
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
     }
+  }
 })
